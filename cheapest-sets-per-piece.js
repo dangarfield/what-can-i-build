76e11e63dc1bcf00cf3a10e2e5cdf477dfa3736b
Dan Garfield <dangarfielduk@hotmail.com>
2022-11-14 21:04:33 +0000

    cheapest sets wip

---
+++ cheapest-sets-per-piece.js
@@ -0,0 +1,119 @@
+// https://www.pricerunner.com/cl/72/Toys?attr_57121593=57121615&attr_57121617=60430082&sort=price_asc
+
+[...document.querySelectorAll('.k6oEmfY83J')].map(p => p.textContent).filter(t => t.includes('Pieces')).map(e => {return {e, no:parseInt(e.split('Pieces')[0].split(' ').filter(t => t !== '').reverse()[0]), p: parseFloat(parseFloat(e.split('£')[1]).toFixed(2))}}).map(a => { return {t:a.e, no:a.no, p:a.p, e:a.p/a.no}}).sort((a,b) => a.e - b.e).map(e => JSON.stringify(e))
+
+{"t":"Lego Technic McLaren Senna GTR 42123Lego Technic, From 10 years, 830 Pieces£34.955.0","no":830,"p":34.95,"e":0.04210843373493976}
+{"t":"Lego Technic Catamaran 42105Lego Technic, From 8 years, 404 Pieces£18.00","no":404,"p":18,"e":0.04455445544554455}
+{"t":"Lego Technic Jeep Wrangler 42122Lego Technic, From 9 years, 665 Pieces£30.00","no":665,"p":30,"e":0.045112781954887216}
+{"t":"Lego Technic The Batman Batmobile 42127Lego Technic, From 10 years, 1360 Pieces£63.00","no":1360,"p":63,"e":0.04632352941176471}
+{"t":"50+ watchingLego Technic Car Transporter 42098Lego Technic, From 11 years, 2493 Pieces£119.954.9","no":2493,"p":119.95,"e":0.04811472121941436}
+{"t":"Lego Technic Rescue Hovercraft 42120Lego Technic, From 8 years, 457 Pieces£22.95","no":457,"p":22.95,"e":0.05021881838074398}
+{"t":"50+ watchingLego Technic Ford F 150 Raptor 42126Lego Technic, From 18 years, 1379 Pieces£69.99","no":1379,"p":69.99,"e":0.050754169688179834}
+{"t":"Lego Technic Motorcycle 42132Lego Technic, From 7 years, 163 Pieces£8.99","no":163,"p":8.99,"e":0.055153374233128834}
+{"t":"Lego Technic Monster Jam Megalodon 42134Lego Technic, From 7 years, 260 Pieces£14.40","no":260,"p":14.4,"e":0.055384615384615386}
+{"t":"Lego Technic Heavy-Duty Excavator 42121Lego Technic, From 8 years, 569 Pieces£31.99","no":569,"p":31.99,"e":0.056221441124780315}
+{"t":"200+ watchingLego Technic Land Rover Defender 42110Lego Technic, From 11 years, 2573 Pieces£144.994.7","no":2573,"p":144.99,"e":0.05635056354450059}
+{"t":"-25%Lego Technic Monster Jam Grave Digger 42118Lego Technic, From 7 years, 212 Pieces£11.99£9.00","no":212,"p":11.99,"e":0.05655660377358491}
+{"t":"Lego Technic Race Plane 42117Lego Technic, From 7 years, 154 Pieces£8.78","no":154,"p":8.78,"e":0.05701298701298701}
+{"t":"Lego Technic Monster Jam™ El Toro Loco™ 42135Lego Technic, From 7 years, 247 Pieces£14.40","no":247,"p":14.4,"e":0.058299595141700404}
+{"t":"Lego Technic Monster Jam Max-D 42119Lego Technic, From 7 years, 230 Pieces£13.50","no":230,"p":13.5,"e":0.058695652173913045}
+{"t":"Lego Technic Stunt Show Truck & Bike 42106Lego Technic, From 8 years, 610 Pieces£35.994.9","no":610,"p":35.99,"e":0.059000000000000004}
+{"t":"50+ watchingLego Technic Heavy Tow Truck 42128Lego Technic, From 11 years, 2017 Pieces£119.991.0","no":2017,"p":119.99,"e":0.0594893406048587}
+{"t":"Lego Technic Mobile Crane 42108Lego Technic, From 10 years, 1292 Pieces£79.954.8","no":1292,"p":79.95,"e":0.061880804953560374}
+{"t":"-20%Lego Technic Telehandler 42133Lego Technic, From 7 years, 143 Pieces£8.95£7.20","no":143,"p":8.95,"e":0.06258741258741259}
+{"t":"100+ watchingLego Technic Ferrari 488 GTE AF Corse #51 42125Lego Technic, From 18 years, 1682 Pieces£105.995.0","no":1682,"p":106,"e":0.06302021403091558}
+{"t":"Lego Technic John Deere 9620R 4WD Tractor 42136Lego Technic, From 8 years, 390 Pieces£24.99","no":390,"p":24.99,"e":0.06407692307692307}
+{"t":"50+ watchingLego Technic Rough Terrain Crane 42082Lego Technic, From 11 years, 4057 Pieces, Theme: Construction Site£265.764.9","no":4057,"p":265.76,"e":0.06550653192013803}
+{"t":"TrendingLego Technic Ford Mustang Shelby GT500 42138Lego Technic, From 9 years, 544 Pieces£35.99","no":544,"p":35.99,"e":0.06615808823529412}
+{"t":"Lego Technic Airbus H175 Rescue Helicopter 42145Lego Technic, From 11 years, 2001 Pieces, 1 pcs£134.99","no":2001,"p":134.99,"e":0.06746126936531735}
+{"t":"50+ watchingLego Technic Dom's Dodge Charger 42111Lego Technic, From 10 years, 1077 Pieces£73.745.0","no":1077,"p":73.75,"e":0.06847725162488394}
+{"t":"Lego Technic Chevrolet Corvette ZR1 42093Lego Technic, From 9 years, 579 Pieces£39.994.7","no":579,"p":39.99,"e":0.06906735751295337}
+{"t":"Lego Technic Mini Claas Xerion 42102Lego Technic, From 7 years, 130 Pieces, Theme: Farm Life£8.995.0","no":130,"p":8.99,"e":0.06915384615384615}
+{"t":"200+ watchingLego Technic Porsche 911 RSR 42096Lego Technic, From 10 years, 1580 Pieces£109.994.8","no":1580,"p":109.99,"e":0.06961392405063291}
+{"t":"200+ watchingLego Technic Lamborghini Sian FKP 37 42115Lego Technic, From 2 years, 3696 Pieces£257.955.0","no":3696,"p":257.95,"e":0.06979166666666667}
+{"t":"Lego Technic All Terrain Vehicle 42139Lego Technic, From 10 years, 764 Pieces£53.99","no":764,"p":53.99,"e":0.0706675392670157}
+{"t":"50+ watchingLego Technic Ducati Panigale V4 R 42107Lego Technic, From 10 years, 646 Pieces£48.775.0","no":646,"p":48.77,"e":0.07549535603715171}
+{"t":"Lego Technic Monster Truck 42005Lego Technic, From 9 years, 329 Pieces£25.00","no":329,"p":25,"e":0.07598784194528875}
+{"t":"Lego Technic Rescue Helicopter 42092Lego Technic, From 8 years, 325 Pieces£24.994.7","no":325,"p":24.99,"e":0.07689230769230769}
+{"t":"Lego Technic Concrete Mixer Truck 42112Lego Technic, From 10 years, 1163 Pieces£89.99","no":1163,"p":89.99,"e":0.0773774720550301}
+{"t":"Lego Technic Formula E Porsche 99X Electric 42137Lego Technic, From 9 years, 422 Pieces£33.49","no":422,"p":33.49,"e":0.07936018957345972}
+{"t":"Lego Technic Dragster 42103Lego Technic, From 7 years, 225 Pieces£17.994.9","no":225,"p":17.99,"e":0.07995555555555554}
+{"t":"Lego Technic Hovercraft 42076Lego Technic, From 9 years, 1020 Pieces£82.99","no":1020,"p":82.99,"e":0.08136274509803922}
+{"t":"50+ watchingLego Technic BMW M 1000 RR 42130Lego Technic, From 18 years, 1920 Pieces£159.00","no":1920,"p":159,"e":0.0828125}
+{"t":"100+ watchingLego Technic App Controlled D11 Bulldozer 42131Lego Technic, From 18 years, 3854 Pieces£319.99","no":3854,"p":319.99,"e":0.08302802283341983}
+{"t":"50+ watchingLego Technic Mercedes-Benz Zetros Trial Truck 42129Lego Technic, From 12 years, 2110 Pieces£175.674.0","no":2110,"p":175.67,"e":0.08325592417061611}
+{"t":"Lego Technic Extreme Adventure 42069Lego Technic, From 11 years, 2382 Pieces£199.954.7","no":2382,"p":199.95,"e":0.08394206549118387}
+{"t":"100+ watchingLego Technic McLaren Formula 1 42141Lego Technic, From 18 years, 1432 Pieces£122.994.0","no":1432,"p":122.99,"e":0.08588687150837988}
+{"t":"Lego Technic Mine Loader 42049Lego Technic, From 9 years, 476 Pieces, Theme: Construction Site£40.95","no":476,"p":40.95,"e":0.08602941176470588}
+{"t":"200+ watchingLego Technic Bugatti Chiron 42083Lego Technic, From 16 years, 3599 Pieces£319.994.7","no":3599,"p":319.99,"e":0.08891080855793276}
+{"t":"200+ watchingLego Technic Ferrari Daytona SP3 42143Lego Technic, From 18 years, 3778 Pieces£339.95","no":3778,"p":339.95,"e":0.08998147167813658}
+{"t":"100+ watchingLego Technic Volvo 6x6 Articulated Truck 42114Lego Technic, From 11 years, 2193 Pieces£201.344.5","no":2193,"p":201.34,"e":0.09181030551755587}
+{"t":"Lego Technic Material Handler 42144Lego Technic, From 10 years, 835 Pieces£78.74","no":835,"p":78.74,"e":0.09429940119760478}
+{"t":"200+ watchingLego Technic Liebherr R 9800 Excavator 42100Lego Technic, From 12 years, 4108 Pieces£399.994.7","no":4108,"p":399.99,"e":0.09736854917234665}
+{"t":"Lego Technic Volvo Concept Wheel Loader ZEUX 42081Lego Technic, From 10 years, 1167 Pieces£114.99","no":1167,"p":114.99,"e":0.09853470437017994}
+{"t":"Lego Technic Volvo Wheel Loader 30433Lego Technic, From 7 years, 69 Pieces£6.99","no":69,"p":6.99,"e":0.10130434782608697}
+{"t":"Lego Technic Hook Loader 42084Lego Technic, From 7 years, 176 Pieces£19.95","no":176,"p":19.95,"e":0.11335227272727272}
+{"t":"Lego Technic Helicopter 30465Lego Technic, From 6 years, 70 Pieces£7.99","no":70,"p":7.99,"e":0.11414285714285714}
+{"t":"Lego Technic First Responder 42075Lego Technic, From 9 years, 513 Pieces, Theme: Fire Fighter£59.05","no":513,"p":59.05,"e":0.11510721247563352}
+{"t":"Lego Technic Rally Car 42077Lego Technic, From 10 years, 1005 Pieces£117.954.7","no":1005,"p":117.95,"e":0.11736318407960199}
+{"t":"TrendingLego Technic App Controlled Transformation Vehicle 42140Lego Technic, From 9 years, 772 Pieces£91.99","no":772,"p":91.99,"e":0.1191580310880829}
+{"t":"Lego Technic Race Truck 42104Lego Technic, From 7 years, 227 Pieces£27.954.7","no":227,"p":27.95,"e":0.12312775330396475}
+{"t":"Lego Technic Compact Crawler Crane 42097Lego Technic, From 10 years, 920 Pieces, Theme: Building£119.904.6","no":920,"p":119.9,"e":0.13032608695652176}
+{"t":"Lego Technic Mack Anthem 42078Lego Technic, From 11 years, 2595 Pieces£339.994.6","no":2595,"p":339.99,"e":0.13101734104046242}
+{"t":"Lego Technic Getaway Truck 42090Lego Technic, From 7 years, 128 Pieces£17.994.4","no":128,"p":17.99,"e":0.140546875}
+{"t":"Lego Technic Volvo EW160E 42053Lego Technic, From 10 years, 1166 Pieces, Theme: Construction Site£172.99","no":1166,"p":172.99,"e":0.14836192109777016}
+{"t":"Lego Technic Forest Machine 42080Lego Technic, From 10 years, 1003 Pieces£148.99","no":1003,"p":148.99,"e":0.14854436689930212}
+{"t":"Lego Technic Cherry Picker 42088Lego Technic, From 7 years, 155 Pieces£23.77","no":155,"p":23.77,"e":0.15335483870967742}
+{"t":"Lego Technic 6x6 All Terrain Tow Truck 42070Lego Technic, From 11 years, 1862 Pieces£294.994.0","no":1862,"p":294.99,"e":0.15842642320085928}
+{"t":"Lego Technic Arctic Truck 42038Lego Technic, From 10 years, 913 Pieces£144.994.5","no":913,"p":144.99,"e":0.15880613362541074}
+{"t":"Lego Technic Heavy Duty Forklift 42079Lego Technic, From 9 years, 592 Pieces£94.495.0","no":592,"p":94.5,"e":0.15962837837837837}
+{"t":"Lego Technic Buggy 42101Lego Technic, From 7 years, 117 Pieces£20.39","no":117,"p":20.39,"e":0.17427350427350427}
+{"t":"Lego Technic Tracked Loader 42094Lego Technic, From 10 years, 827 Pieces, Theme: Construction Site£144.99","no":827,"p":144.99,"e":0.17532043530834343}
+{"t":"Lego Technic Mobile Crane MK II 42009Lego Technic, From 11 years, 2606 Pieces, Theme: Construction Site£475.004.8","no":2606,"p":475,"e":0.1822716807367613}
+{"t":"Lego Technic Mercedes-Benz Arocs 3245 42043Lego Technic, From 11 years, 2793 Pieces£520.004.7","no":2793,"p":520,"e":0.1861797350519155}
+{"t":"Lego Technic Customized Pick up Truck 42029Lego Technic, From 10 years, 1063 Pieces£199.99£189.994.7","no":1063,"p":199.99,"e":0.188137347130762}
+{"t":"Lego Technic Hot Rod 42022Lego Technic, From 9 years, 414 Pieces£83.08","no":414,"p":83.08,"e":0.20067632850241546}
+{"t":"100+ watchingLego Technic 4x4 X Treme Off Roader 42099Lego Technic, From 11 years, 958 Pieces£199.994.5","no":958,"p":199.99,"e":0.2087578288100209}
+{"t":"Lego Technic Remote Controlled Stunt Racer 42095Lego Technic, From 9 years, 324 Pieces£69.995.0","no":324,"p":70,"e":0.21604938271604937}
+{"t":"Lego Technic Formula Off-Roader 42037Lego Technic, From 9 years, 494 Pieces£106.964.8","no":494,"p":106.96,"e":0.21651821862348178}
+{"t":"50+ watchingLego Technic Off-Road Buggy 42124Lego Technic, From 10 years, 374 Pieces£84.95","no":374,"p":84.95,"e":0.22713903743315508}
+{"t":"Lego Technic Bucket Wheel Excavator 42055Lego Technic, From 11 years, 3929 Pieces, Theme: Construction Site£901.504.6","no":3929,"p":901.5,"e":0.22944769661491474}
+{"t":"Lego Technic Drag Racer 42050Lego Technic, From 10 years, 647 Pieces£149.994.7","no":647,"p":149.99,"e":0.23182380216383308}
+{"t":"Lego Technic BMW R 1200 GS Adventure 42063Lego Technic, From 10 years, 603 Pieces£142.495.0","no":603,"p":142.5,"e":0.236318407960199}
+{"t":"Lego Technic Police Interceptor 42047Lego Technic, From 7 years, 185 Pieces£45.36","no":185,"p":45.36,"e":0.2451891891891892}
+{"t":"Lego Technic Snowmobile 42021Lego Technic, From 8 years, 186 Pieces£47.144.8","no":186,"p":47.14,"e":0.25344086021505374}
+{"t":"Lego Technic Container Truck 42024Lego Technic, From 10 years, 948 Pieces, Theme: Construction Site£244.004.7","no":948,"p":244,"e":0.25738396624472576}
+{"t":"Lego Technie Roadwork Crew 42060Lego Technic, From 8 years, 365 Pieces£94.17","no":365,"p":94.17,"e":0.258}
+{"t":"Lego Technic App Controlled Top Gear Rally Car 42109Lego Technic, From 9 years, 463 Pieces£119.954.3","no":463,"p":119.95,"e":0.25907127429805615}
+{"t":"Lego Technic Record Breaker 42033Lego Technic, From 7 years, 125 Pieces£32.904.8","no":125,"p":32.9,"e":0.2632}
+{"t":"Lego Technic Hydroplane Racer 42045Lego Technic, From 8 years, 180 Pieces£48.484.6","no":180,"p":48.48,"e":0.2693333333333333}
+{"t":"Lego Technic Remote Controlled Volvo L350F Wheel Loader 42030Lego Technic, From 11 years, 1636 Pieces£448.994.9","no":1636,"p":448.99,"e":0.2744437652811736}
+{"t":"Lego Technic Excavator 42006Lego Technic, From 10 years, 720 Pieces, Theme: Construction Site£199.995.0","no":720,"p":200,"e":0.2777777777777778}
+{"t":"Lego Technic Ocean Explorer 42064Lego Technic, From 10 years, 1327 Pieces£376.734.7","no":1327,"p":376.73,"e":0.283896006028636}
+{"t":"Lego Technic Stunt Bike 42058Lego Technic, From 7 years, 140 Pieces£39.79","no":140,"p":39.79,"e":0.2842142857142857}
+{"t":"Lego Technic Power Boat 42089Lego Technic, From 8 years, 174 Pieces£49.99","no":174,"p":49.99,"e":0.28729885057471266}
+{"t":"Lego Technic Police Pursuit 42091Lego Technic, From 7 years, 120 Pieces, Theme: Police£35.974.8","no":120,"p":35.97,"e":0.29975}
+{"t":"Lego Technic Telehandler 42061Lego Technic, From 9 years, 260 Pieces£77.99","no":260,"p":77.99,"e":0.29996153846153845}
+{"t":"Lego Technic Fire Plane 42040Lego Technic, From 9 years, 578 Pieces, Theme: Fire Fighter£174.674.9","no":578,"p":174.67,"e":0.30219723183391}
+{"t":"Lego Technic 24 Hours Race Car 42039Lego Technic, From 11 years, 1219 Pieces£375.504.8","no":1219,"p":375.5,"e":0.308039376538146}
+{"t":"Lego Technic Compact Tracked Loader 42032Lego Technic, From 7 years, 145 Pieces, Theme: Construction Site£44.904.7","no":145,"p":44.9,"e":0.3096551724137931}
+{"t":"Lego Technic Getaway Racer 42046Lego Technic, From 7 years, 170 Pieces£55.334.8","no":170,"p":55.33,"e":0.3254705882352941}
+{"t":"Lego Technic Porsche 911 GT3 RS 42056Lego Technic, From 16 years, 2704 Pieces£899.994.7","no":2704,"p":899.99,"e":0.3328365384615385}
+{"t":"Lego Technic Heavy Lift Helicopter 42052Lego Technic, From 10 years, 1043 Pieces£352.25","no":1043,"p":352.25,"e":0.33772770853307765}
+{"t":"Lego Technic Cherry Picker 42031Lego Technic, From 7 years, 155 Pieces£53.524.7","no":155,"p":53.52,"e":0.3452903225806452}
+{"t":"Lego Technic Service Truck 42008Lego Technic, From 10 years, 1224 Pieces£427.504.6","no":1224,"p":427.5,"e":0.3492647058823529}
+{"t":"Lego Technic Street Motorcycle 42036Lego Technic, From 9 years, 375 Pieces£134.994.7","no":375,"p":134.99,"e":0.35997333333333337}
+{"t":"Lego Technic Ultralight Helicopter 42057Lego Technic, From 8 years, 199 Pieces£75.324.0","no":199,"p":75.32,"e":0.37849246231155775}
+{"t":"Lego Technic Stunt Truck 42059Lego Technic, From 7 years, 142 Pieces£54.99","no":142,"p":54.99,"e":0.3872535211267606}
+{"t":"Lego Techni Whack 42072Lego Technic, From 7 years, 135 Pieces, Theme: Building£53.52","no":135,"p":53.52,"e":0.3964444444444445}
+{"t":"Lego Technic Display Team Jet 42044Lego Technic, From 7 years, 113 Pieces£47.50","no":113,"p":47.5,"e":0.42035398230088494}
+{"t":"Lego Technic Bell Boeing V22 Osprey 42113Lego Technic, From 11 years, 1636 Pieces£699.99","no":1636,"p":699.99,"e":0.4278667481662592}
+{"t":"Lego Technic Bash! 42073Lego Technic, From 7 years, 139 Pieces£73.83£69.54","no":139,"p":73.83,"e":0.5311510791366907}
+{"t":"Lego Technic Off-road Racer 42010Lego Technic, From 7 years, 160 Pieces£86.65","no":160,"p":86.65,"e":0.5415625000000001}
+{"t":"Lego Technic Quad Bike 42034Lego Technic, From 7 years, 148 Pieces£87.505.0","no":148,"p":87.5,"e":0.5912162162162162}
+{"t":"Lego Technic Dozer Compactor 42071Lego Technic, From 8 years, 171 Pieces, Theme: Building£115.91","no":171,"p":115.91,"e":0.6778362573099415}
+{"t":"Lego Technic Black Champion Racer 42026Lego Technic, From 7 years, 138 Pieces£153.904.8","no":138,"p":153.9,"e":1.115217391304348}
+{"t":"Lego Technic Powered Up Large Motor 88013Lego Technic, From 7 years, 1 Pieces£23.99","no":1,"p":23.99,"e":23.99}
+{"t":"Lego Technic XL Motor Powered Up 88014Lego Technic, From 7 years, 1 Pieces£25.49","no":1,"p":25.49,"e":25.49}
+{"t":"Lego Technic Power Functions L Motor 88003Lego Technic, From 7 years, 1 Pieces£27.90","no":1,"p":27.9,"e":27.9}
+{"t":"Lego Technic Power Functions Motor Set 8293Lego Technic, From 9 years, 10 Pieces£343.004.5","no":10,"p":343,"e":34.3}
